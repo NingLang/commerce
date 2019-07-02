7bfa6e14a9691b26323035706741619f26e561b8
NingLang <mr.langer0622@gmail.com>
2019-07-02 19:55:35 +0800

    analyse delete

--- analyse/page/src/main/scala/com/atguigu/page/DataModel.scala
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2017. Atguigu Inc. All Rights Reserved.
- * Date: 10/28/17 9:52 AM.
- * Author: wuyufei.
- */
-
-package com.atguigu.page
-
-case class PageSplitConvertRate(taskid: String, convertRate: String)
